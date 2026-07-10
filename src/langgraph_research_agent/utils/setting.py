d5e0d06c507e8816f96189b7ba2a1122c433c2a8
Frederic Dong <frederic.dong@epita.fr>
2026-07-10 10:06:44 +0200

    :feat: added new utils

--- src/langgraph_research_agent/utils/setting.py
+++ src/langgraph_research_agent/utils/setting.py
@@ -6,5 +6,5 @@ load_dotenv()
 
 client_path = Path(os.getenv("client_path") or "workspace/chroma")
 collection_name = os.getenv("collection_name") or "agent_memory"
-openapi_key=os.getenv("OPENAI_API_KEY")
+openapi_key = os.getenv("OPENAI_API_KEY")
 workspace = Path(os.getenv("workspace") or "workspace").resolve()
